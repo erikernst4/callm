f1c565a0d9e96a78e8105511d76c26e6e6e994c1
Erik Ernst <erik_ernst@hotmail.com.ar>
2026-01-11 15:17:57 -0300

    refactor: separate validation and evaluation configs

--- main.py
+++ main.py
@@ -1,24 +1,18 @@
-from callm.models.llm import LLM
-from callm.data.triviaQA import TriviaQADataModule
 from cli import CalibrationCLI
 
 
 def cli_main():
-    cli = CalibrationCLI(
-        LLM,
-        TriviaQADataModule,
+    CalibrationCLI(
         parser_kwargs={
-            "validate": {"default_config_files": ["configs/config_base.yaml"]},
+            "validate": {
+                "default_config_files": ["configs/config_base_validation.yaml"]
+            },
+            "evaluation": {
+                "default_config_files": ["configs/config_base_evaluation.yaml"]
+            },
             "parser_mode": "omegaconf",
         },
     )
-
-    if hasattr(cli.model, "tokenizer") and hasattr(cli.model, "model_name"):
-        if cli.datamodule.model_name != cli.model.model_name:
-            raise ValueError(
-                f"Data module model_name ({cli.datamodule.model_name}) doesn't match "
-                f"model model_name ({cli.model.model_name})."
-            )
 
 
 if __name__ == "__main__":
