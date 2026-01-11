83e50923c5bc04412d90da5e9358c46a2c805e5d
Erik Ernst <erik_ernst@hotmail.com.ar>
2026-01-11 15:09:42 -0300

    refactor: export classes from modules

---
+++ callm/data/__init__.py
@@ -0,0 +1,4 @@
+from .triviaQA import TriviaQADataModule
+from .evaluator_data import EvaluatorDataModule
+
+__all__ = ["TriviaQADataModule", "EvaluatorDataModule"]
---
+++ callm/models/__init__.py
@@ -0,0 +1,4 @@
+from .llm import LLM
+from .evaluator import EvaluatorModule
+
+__all__ = ["LLM", "EvaluatorModule"]
